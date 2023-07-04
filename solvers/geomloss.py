8a9fa77e691ed9d12bc8d7512cc2708cc7b8a9e7
Jean Feydy <jean.feydy@gmail.com>
2023-07-04 17:18:38 +0200

    Minor update.

--- solvers/geomloss.py
+++ solvers/geomloss.py
@@ -25,7 +25,7 @@ class Solver(BaseSolver):
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
     parameters = {
-        'blur': [1e-4, 1e-3, 1e-2, 1e-1],
+        'blur': [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6],
         'use_gpu': [True, False],
     }
 
